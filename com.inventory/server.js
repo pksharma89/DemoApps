c00db9a3ebe8b61829ee4c388b8c88b0ff68045f
Piyush Sharma <piyush@Piyushs-MacBook-Air.local>
2017-11-22 17:05:10 +0530

    Testing

--- com.inventory/server.js
+++ com.inventory/server.js
@@ -2,3 +2,8 @@
 const mongodb = require('mongodb');
 const express = require('express')
 const app = express()
+
+const MongoClient = mongodb.MongoClient
+const url = "mongodb://localhost:27017/Northwind"
+
+app.use(express.static('public'))
